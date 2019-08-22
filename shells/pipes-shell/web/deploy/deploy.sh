18f610dfda7a8106bf1def18c97725106ca6c679
Cameron Silvestrini <csilvestrini@google.com>
2019-08-22 10:21:15 -0700

    Fix the pipes-shell deploy script

--- shells/pipes-shell/web/deploy/deploy.sh
+++ shells/pipes-shell/web/deploy/deploy.sh
@@ -7,19 +7,7 @@ cp -fR source/index.html dist/
 cp -fR ../../../lib/build/worker.js dist/
 # particles
 mkdir dist/particles
-#
-mkdir dist/particles/Common
-mkdir dist/particles/Common/schema
-cp -fR ../../../../particles/Common/schema/Description.schema dist/particles/Common/schema
-#
-mkdir dist/particles/PipeApps
-cp -fR ../../../../particles/PipeApps/* dist/particles/PipeApps
-#
-mkdir dist/particles/Music
-cp -fR ../../../../particles/Music/* dist/particles/Music
-#
-mkdir dist/particles/Services
-cp -fR ../../../../particles/Services/* dist/particles/Services
+cp -fR ../../../../particles/* dist/particles
 #
 # collate sources
 echo packing...
