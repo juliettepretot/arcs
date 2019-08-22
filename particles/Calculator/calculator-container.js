52f5515e69d027e030f43eb9dbea3b488849627c
Jason Feinstein <jwf@google.com>
2019-08-22 15:06:12 -0700

    Fix layout

--- particles/Calculator/calculator-container.js
+++ particles/Calculator/calculator-container.js
@@ -2,8 +2,10 @@ defineParticle(({DomParticle, html}) => {
   return class extends DomParticle {
     get template() {
       return html`
-        <span slotid="lhsTerm"></span><span slotid="operatorSlot"></span><span slotid="rhsTerm"></span>
-        <span slotid="resultSlot"></span>
+        <span slotid="lhsTerm" style="display: inline-block"></span>
+        <span slotid="operatorSlot" style="display: inline-block"></span>
+        <span slotid="rhsTerm" style="display: inline-block"></span>
+        = <span slotid="resultSlot" style="display: inline-block"></span>
       `;
     }
   };
