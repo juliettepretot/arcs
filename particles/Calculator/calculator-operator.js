71b8703abebc8d3d95c1fb7ed53478f0f7fd0ced
Jason Feinstein <jwf@google.com>
2019-08-23 13:23:01 -0700

    Make the operator dropdown respond to the operator handle.

--- particles/Calculator/calculator-operator.js
+++ particles/Calculator/calculator-operator.js
@@ -2,7 +2,7 @@ defineParticle(({DomParticle, html}) => {
   return class extends DomParticle {
     get template() {
       return html`
-        <select id="operation" on-change="onChange">
+        <select id="operation" on-change="onChange" selectedIndex="{{selectedIndex:modelIndex}}">
           <option value="add">+</option>
           <option value="divide">-</option>
           <option value="multiply">*</option>
@@ -13,6 +13,28 @@ defineParticle(({DomParticle, html}) => {
 
     setHandles(handles) {
       this.operatorHandle = handles.get('operator');
+    }
+
+    render({operator}) {
+      var modelIndex = 0
+      if (operator && operator.operator) {
+        switch (operator.operator) {
+          case 'add':
+            modelIndex = 0; break;
+          case 'subtract':
+            modelIndex = 1; break;
+          case 'multiply':
+            modelIndex = 2; break;
+          case 'divide':
+            modelIndex = 3; break;
+        }
+      }
+      return {
+        modelIndex: modelIndex
+        //{
+        //  modelIndex: modelIndex
+        //}
+      };
     }
 
     onChange(e) {
