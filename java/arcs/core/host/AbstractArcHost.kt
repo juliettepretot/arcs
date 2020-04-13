82a5b2136ebe388e2a3284a95a59bd7b499f610d
Alex Rosengarten <alxrsngrtn@google.com>
2020-04-13 11:52:38 -0700

    Sealed constructor class approach

--- java/arcs/core/host/AbstractArcHost.kt
+++ java/arcs/core/host/AbstractArcHost.kt
@@ -33,7 +33,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-typealias ParticleConstructor = suspend () -> Particle
+sealed class ParticleConstructor {
+    // TODO(alxr): Do these need to be suspend functions? If so, I have a solution to achieve that.
+    abstract class Empty : () -> Particle, ParticleConstructor()
+    abstract class Spec : (Plan.Particle) -> Particle, ParticleConstructor()
+}
+
 typealias ParticleRegistration = Pair<ParticleIdentifier, ParticleConstructor>
 
 /** Maximum number of times a particle may fail to be started before giving up. */
@@ -57,6 +62,7 @@ abstract class AbstractArcHost(
     private val log = TaggedLog { "AbstractArcHost" }
     protected val particleConstructors: MutableMap<ParticleIdentifier, ParticleConstructor> =
         mutableMapOf()
+
     /** In memory cache of [ArcHostContext] state. */
     private val contextCache: MutableMap<String, ArcHostContext> = LruCacheMap()
 
@@ -67,6 +73,7 @@ abstract class AbstractArcHost(
 
     // TODO: refactor to allow clients to supply this
     private val coroutineContext = Dispatchers.Unconfined + CoroutineName("AbstractArcHost")
+
     // TODO: add lifecycle API for ArcHosts shutting down to cancel running coroutines
     private val scope = CoroutineScope(coroutineContext)
 
@@ -514,9 +521,11 @@ abstract class AbstractArcHost(
         identifier: ParticleIdentifier,
         spec: Plan.Particle
     ): Particle {
-        return particleConstructors[identifier]?.invoke() ?: throw IllegalArgumentException(
-            "Particle $identifier not found."
-        )
+        return when (val constructor = particleConstructors[identifier]) {
+            is ParticleConstructor.Empty -> constructor.invoke()
+            is ParticleConstructor.Spec -> constructor.invoke(spec)
+            else -> throw IllegalArgumentException("Particle $identifier not found.")
+        }
     }
 
     companion object {
